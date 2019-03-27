1d28cf3d44c44743e1f18df7843b0c69ede45a28
jacobmix <jacobmix@users.noreply.github.com>
2019-03-28 00:03:54 +0100

    Update moonlight.sh

--- moonlight.sh
+++ moonlight.sh
@@ -52,6 +52,7 @@ function update_pakages {
 	INSTALL_PKGS=" libopus0 libexpat1 libasound2 libudev0 or libudev1 libavahi-client3 libcurl3 libevdev2 libenet7 rbp-userland-osmc libraspberrypi0 libssl-dev libopus-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libevdev-dev libexpat1-dev libpulse-dev uuid-dev libenet-dev cmake gcc g++ libraspberrypi-dev fakeroot debhelper "
 	for i in $INSTALL_PKGS; do
 	sudo apt-get install -y $i
+done
 }
 
 # build moonlight from master
